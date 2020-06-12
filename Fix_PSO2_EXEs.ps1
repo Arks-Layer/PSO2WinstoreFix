961224642fcd7a8695693e075ca2c4ae65c15f49
Alam Arias <Alam.GBC+Git@gmail.com>
2020-06-11 21:49:50 -0400

    Create Fix_PSO2_EXEs.ps1

---
+++ Fix_PSO2_EXEs.ps1
@@ -0,0 +1,24 @@
+﻿$UpdateState = @()
+If (-Not ( Test-Path -Path "client_na.json" -PathType Leaf))
+{
+"client_na.json file is missing, please move me into the Tweaker folder"
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+exit 0
+}
+"Loading Tweaker's NA Update state file, client_na.json"
+$UpdateState += Get-Content -Raw -Path "client_na.json" | ConvertFrom-Json
+$BadEXEs = @()
+"Selecting EXEs listings"
+$BadEXEs += $UpdateState | Get-Member -Name "*.exe"
+If ($BadEXEs.Count -gt 0)
+{
+"Removing EXEs listing"
+$NewState = $UpdateState | Select-Object -Property * -ExcludeProperty $BadEXEs.Name
+}
+Else
+{
+"No EXE listing found"
+$NewState = $UpdateState
+}
+"Saving to Tweaker's NA Update state file, client_na.json"
+$NewState  | ConvertTo-Json | Out-File  -FilePath "client_na.json"
