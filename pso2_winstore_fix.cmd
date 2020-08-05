2c9820addb9471be95fdee31da9649ebef46b798
Alam Arias <Alam.GBC+Git@gmail.com>
2020-08-05 01:10:09 -0400

    Update pso2_winstore_fix.cmd
    Let list the PS1 files

--- pso2_winstore_fix.cmd
+++ pso2_winstore_fix.cmd
@@ -5,5 +5,6 @@ IF EXIST "%ffp%" (
  start "Kickstarting PowerShell Script: %~n0" /D "%pathdir%" /LOW /WAIT cmd /C powershell.exe -NoLogo -NoExit -NoProfile -ExecutionPolicy ByPass -File "%ffp%"
 ) ELSE (
  echo Where is the PowerShell script file?, I could not find: %ffp%
+ dir /B "%pathdir%*.ps1"
  pause
 )
